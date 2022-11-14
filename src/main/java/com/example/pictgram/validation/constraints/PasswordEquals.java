65b71fc78f981b926cfd03eec9531119887d8eb4
rinrin <r.chaaaaaan@gmail.com>
2022-11-15 03:25:16 +0900

    update

--- src/main/java/com/example/pictgram/validation/constraints/PasswordEquals.java
+++ src/main/java/com/example/pictgram/validation/constraints/PasswordEquals.java
@@ -1,10 +1,11 @@
 package com.example.pictgram.validation.constraints;
 
-import java.long.annotation.Documented;
-import java.long.annotation.ElementType;
-import java.long.annotation.Retention;
-import java.long.annotation.RetentionPolicy;
-import java.long.annotation.Target;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
 
